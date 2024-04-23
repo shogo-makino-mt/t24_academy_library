637e370ff9ab2e3235f472d33a5c0c18f478f8af
shogo-makino-mt <shogo.makino@metateam.co.jp>
2024-04-23 14:44:09 +0900

    貸出一覧、貸出登録機能を追加

--- src/main/java/jp/co/metateam/library/controller/RentalManageController.java
+++ src/main/java/jp/co/metateam/library/controller/RentalManageController.java
@@ -1,15 +1,23 @@
 package jp.co.metateam.library.controller;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.validation.BindingResult;
 import jp.co.metateam.library.service.AccountService;
 import jp.co.metateam.library.service.RentalManageService;
 import jp.co.metateam.library.service.StockService;
 import lombok.extern.log4j.Log4j2;
-
+import jp.co.metateam.library.model.RentalManage;
+import jp.co.metateam.library.model.RentalManageDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import jakarta.validation.Valid;
+import jp.co.metateam.library.values.RentalStatus;
+import jp.co.metateam.library.service.BookMstService;
+import jp.co.metateam.library.model.BookMst;
 /**
  * 貸出管理関連クラスß
  */
@@ -20,16 +28,18 @@ public class RentalManageController {
     private final AccountService accountService;
     private final RentalManageService rentalManageService;
     private final StockService stockService;
-
+    private final BookMstService bookMstService;
     @Autowired
     public RentalManageController(
         AccountService accountService, 
         RentalManageService rentalManageService, 
-        StockService stockService
+        StockService stockService,
+        BookMstService bookMstService
     ) {
         this.accountService = accountService;
         this.rentalManageService = rentalManageService;
         this.stockService = stockService;
+        this.bookMstService = bookMstService;
     }
 
     /**
@@ -37,14 +47,52 @@ public class RentalManageController {
      * @param model
      * @return
      */
-    @GetMapping("/rental/index")
+    @GetMapping("/rental/index") 
     public String index(Model model) {
-        // 貸出管理テーブルから全件取得
-
-        // 貸出一覧画面に渡すデータをmodelに追加
-
-        // 貸出一覧画面に遷移
-        return "";
+     //貸出管理テーブルから全取得
+     List<RentalManage> rentalManageList = this.rentalManageService.findAll();
+     //貸出一覧画面に渡すデータをmodelに追加
+     model.addAttribute("rentalManageList", rentalManageList);
+     //貸出一覧画面に遷移
+     return "rental/index";
     }
 
-}
+    @GetMapping("/rental/add")
+    public String add(Model model,@ModelAttribute RentalManageDto rentalManageDto) {
+        List<BookMst> bookMstList = this.bookMstService.findAll();
+        model.addAttribute("bookMstList", bookMstList);
+        model.addAttribute("RentalStatus", RentalStatus.values());
+
+        if (!model.containsAttribute("RentalManageDto")) {
+           model.addAttribute("RentalManageDto", new RentalManageDto());
+        }
+
+        return "rental/add";
+    }
+
+
+
+    @PostMapping("/rental/add")
+    public String save(@Valid @ModelAttribute RentalManageDto rentalManageDto, BindingResult result, RedirectAttributes ra) {
+        try {
+            if (result.hasErrors()) {
+                throw new Exception("Validation error.");
+            }
+            // 登録処理
+            this.rentalManageService.save(rentalManageDto);
+
+            return "redirect:/rental/index";
+        } catch (Exception e) {
+            log.error(e.getMessage());
+
+            ra.addFlashAttribute("rentalManageDto", rentalManageDto);
+            ra.addFlashAttribute("org.springframework.validation.BindingResult.rentalManageDto", result);
+
+            return "redirect:/rental/add";
+        }
+    }
+
+
+}      
+    
+        
